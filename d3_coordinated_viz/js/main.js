645f092ddb9072bffc8c8cc1ca045b82144f24c1
aormisto <35901564+aormisto@users.noreply.github.com>
2018-04-07 18:43:24 -0500

    2-3-Joined CSV
    CSV joined to shp files

--- d3_coordinated_viz/js/main.js
+++ d3_coordinated_viz/js/main.js
@@ -31,6 +31,7 @@ function setMap(){
         .defer(d3.json, "data/countries.topojson") //load choropleth spatial data
         .await(callback);
 	
+	
 	function callback(error, csvData, world, climate){
 		var graticule = d3.geoGraticule()
             .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
@@ -49,13 +50,37 @@ function setMap(){
             .attr("class", "gratLines") //assign class for styling
             .attr("d", path); //project graticule lines
 		
-		
-		
-		
         //translate world and climate countries to TopoJSON
         var worldCountries = topojson.feature(world, world.objects.WorldCountries),
             climateRegions = topojson.feature(climate, climate.objects.countries).features;
+		
+		
+			//variables for data join
+		var attrArray = ["varA", "varB", "varC", "varD", "varE", "varF", "varG", "varH", "varI", "varJ", "varK", "varL", "varM", "varN"];
 
+		//loop through csv to assign each set of csv attribute values to geojson region
+		for (var i=0; i<csvData.length; i++){
+			var csvRegion = csvData[i]; //the current region
+			var csvKey = csvRegion.ADMIN; //the CSV primary key
+
+			//loop through geojson regions to find correct region
+			for (var a=0; a<climateRegions.length; a++){
+
+				var geojsonProps = climateRegions[a].properties; //the current region geojson properties
+				var geojsonKey = geojsonProps.ADMIN; //the geojson primary key
+
+				//where primary keys match, transfer csv data to geojson properties object
+				if (geojsonKey == csvKey){
+
+					//assign all attributes and values
+					attrArray.forEach(function(attr){
+						var val = parseFloat(csvRegion[attr]); //get csv attribute value
+						geojsonProps[attr] = val; //assign attribute and value to geojson properties
+					});
+				};
+			};
+		};
+		
                //add world countries to map
         var countries = map.append("path")
             .datum(worldCountries)
