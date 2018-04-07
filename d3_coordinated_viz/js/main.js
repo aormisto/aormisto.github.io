683a62ef9cae24aa22ebd68f0510e4a3846a5fec
aormisto <35901564+aormisto@users.noreply.github.com>
2018-04-07 10:06:56 -0500

    D3 Map works!
    weird gray artifacts within map

--- d3_coordinated_viz/js/main.js
+++ d3_coordinated_viz/js/main.js
@@ -3,8 +3,8 @@ window.onload = setMap();
 function setMap(){
 
     //map frame dimensions
-    var width = 960,
-        height = 460;
+    var width = 1100,
+        height = 500;
 
     //create new svg container for the map
     var map = d3.select("body")
@@ -15,10 +15,10 @@ function setMap(){
 
     //create Albers equal area conic projection 
     var projection = d3.geoAlbers()
-        .center([0, 0])
+        .center([15, 30])
         .rotate([-2, 0, 0])
-        .parallels([43, 62])
-        .scale(2500)
+        .parallels([15, 15])
+        .scale(250)
         .translate([width / 2, height / 2]);
 	
 	var path = d3.geoPath()
