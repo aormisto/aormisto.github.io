34cee9a0dc2026cfd2777d2e73a79431598a1037
aormisto <35901564+aormisto@users.noreply.github.com>
2018-05-12 15:29:57 -0500

    Fullscreen is working!!!
    Fullscreen!!!!!!!!!!!!!

--- Final_Project/js/geojson.js
+++ Final_Project/js/geojson.js
@@ -103,6 +103,7 @@ function createMap(){
 	};
 	L.control.layers(null, overlayMaps).addTo(map);
 	
+	
 	var osmUrl='https://api.mapbox.com/styles/v1/aormisto/cjdzbihla1gtr2tq7c8e6kx1x/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW9ybWlzdG8iLCJhIjoiY2pkemFkcW1wNWY3aDJ4cDBybnp0OHI4cCJ9.5KX05b-V3tmUBACjkVyMMg';
 	var osmAttrib='Map data &copy; OpenStreetMap contributors';
 	var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 15, attribution: osmAttrib});
@@ -111,6 +112,9 @@ function createMap(){
 	
 	var zoomHome = L.Control.zoomHome();
 	zoomHome.addTo(map);
+	
+	map.addControl(new L.Control.Fullscreen());
+
 	
 	//THE FIFTH OPERATOR: Plugin magic goes here! Note that you cannot use the same layer object again, as that will confuse the two map controls
 	var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
